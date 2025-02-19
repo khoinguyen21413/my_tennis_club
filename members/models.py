eda43771991f9fedb9e1610e66a348f2ec03f8d0
DucVan <ducvn.dev@gmail.com>
2025-02-19 19:41:02 +0700

    comment member model

--- members/models.py
+++ members/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
+    # Define field
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
